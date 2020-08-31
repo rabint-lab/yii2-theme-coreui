d7d48d3de64107c4a10e1ea52f3766ac84805a57
ingen <akbarzadeh.mojtaba@gmail.com>
2020-08-31 12:39:35 +0430

    رفع مشکل فوتر در لی اوت فرانت

--- views/layouts/front.php
+++ views/layouts/front.php
@@ -249,11 +249,12 @@ $bundleBaseUrl .= '/dist/';
                 </div>
             </main>
         </div>
-        <?= $this->render('element/decryptModal');?>
         <footer class="c-footer">
             <div><a href="https://coreui.io">CoreUI</a> © 2020 creativeLabs.</div>
             <div class="mfs-auto">Powered by&nbsp;<a href="https://coreui.io/pro/">CoreUI Pro</a></div>
         </footer>
+        <?= $this->render('element/decryptModal');?>
+
     </div>
 
 <?php $this->endContent(); ?>
