b1503acdc59d88981c36acc1b5ffe4d9ef3019d3
rsh <rsh@falinoos.com>
2021-05-12 13:37:08 +0430

    تغییرات صفحه لاگین ifum

--- views/layouts/login.php
+++ views/layouts/login.php
@@ -10,34 +10,34 @@
 
 <div class="container-fluid">
     <div class="row justify-content-start">
-
-        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-8 pr-0">
-            <div class="col-md-12">
-                <?= $this->render('element/_flashes', ['this', $this]) ?>
-            </div>
-            <div class="card mx-4">
-                <div class="card-header">
-                <div class="logo-images">
-                        <a href="https://www.um.ac.ir">
-                            <img src="/img/sponsor.png" alt="">
-                        </a>
-                        <a href="https://">
-                            <img src="" alt="">
-                        </a>
-                    </div>
-                    <!-- <h4 class="text-center color-c mb-2">سامانه دانشجويان بین المللی</h4>
-                    <h6 class="text-center mb-2 mt-2">(دانشگاه فردوسی مشهد)</h6> -->
-                    <h6 class="text-center">
-                        <?= $this->title; ?>
-                    </h6>
-                   
+        <div class="main-card-login" style="width: 100% !important;max-height: 100vh;overflow-y: scroll;">
+            <div class="col-xl-3 col-lg-5 col-md-6 col-sm-8 mt-4 pl-0">
+                <div class="col-md-12">
+                    <?= $this->render('element/_flashes', ['this', $this]) ?>
                 </div>
-                <div class="card-body p-4">
-                    <?php echo $content; ?>
+                <div class="card">
+                    <div class="card-header mt-2">
+                    <div class="logo-images d-none">
+                            <a href="https://www.um.ac.ir">
+                                <img src="/img/sponsor.png" alt="">
+                            </a>
+                            <a href="https://">
+                                <img src="" alt="">
+                            </a>
+                        </div>
+                        <!-- <h4 class="text-center color-c mb-2">سامانه دانشجويان بین المللی</h4>
+                        <h6 class="text-center mb-2 mt-2">(دانشگاه فردوسی مشهد)</h6> -->
+                        <h6 class="text-center">
+                            <?= $this->title; ?>
+                        </h6>
+                    
+                    </div>
+                    <div class="card-body p-4">
+                        <?php echo $content; ?>
+                    </div>
                 </div>
             </div>
         </div>
-
     </div>
 </div>
 <?php $this->endContent(); ?>
